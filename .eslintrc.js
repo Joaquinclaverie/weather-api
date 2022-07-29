d8ded3781a3363765dabf35a90c36a70fbf81341
Joaquin Claverie <50393614+Joaquinclaverie@users.noreply.github.com>
2022-07-29 05:20:52 -0300

    Agrego Tests y falta Readme y Variables de entorno

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-console": 1,
+        "no-debugger": 1,
+        "no-unreachable": 1,
+        "no-unused-vars": 1
+    }
+}
---
+++ tests/routes.test.js
@@ -0,0 +1,46 @@
+const request = require('supertest')
+const baseURL = "http://localhost:3000"
+
+describe('Get Citys', () => {
+    it('Recuperando Ciudades favoritas', async () => {
+        const res = await request(baseURL).get('/v1/citys')
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('ciudadesFavoritas')
+    })
+})
+
+describe('Get Current sin pasarle ciudad', () => {
+    it('Recuperando data de ubicacion y tiempo actual', async () => {
+      const res = await request(baseURL).get('/v1/current')
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('dataUbicacion')
+      expect(res.body).toHaveProperty('tiempoActual')
+    })
+})
+
+describe('Get Current pasandole ciudad', () => {
+    it('Recuperando data de ubicacion y tiempo actual', async () => {
+        const res = await request(baseURL).get('/v1/current/Ushuaia')
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('dataUbicacion')
+        expect(res.body).toHaveProperty('tiempoActual')
+    })
+})
+
+describe('Get Forecast sin pasarle ciudad', () => {
+    it('Recuperando data de ubicacion y tiempo a 5 dias', async () => {
+      const res = await request(baseURL).get('/v1/forecast')
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('dataUbicacion')
+      expect(res.body).toHaveProperty('tiempoACincoDias')
+    })
+})
+
+describe('Get Forecast pasandole ciudad', () => {
+    it('Recuperando data de ubicacion y tiempo a 5 dias', async () => {
+        const res = await request(baseURL).get('/v1/forecast/Ushuaia')
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('dataUbicacion')
+        expect(res.body).toHaveProperty('tiempoACincoDias')
+    })
+})
